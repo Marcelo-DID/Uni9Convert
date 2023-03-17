7490fb3cf5cf8670327f3e4927d15932106f992c
Marcelo Oliveira <marcelo14axl@gmail.com>
2023-03-17 20:35:57 -0300

    Explicações

---
+++ classe.js
@@ -0,0 +1,33 @@
+// class Matematica {
+//     constructor(num1, num2){
+//         num1 = num1,
+//         num2 = num2
+//     }
+
+//     cadastro(num1, num2) {
+//         return num1 + num2;
+//     }
+
+//     subtracao(num1, num2) {
+//         return num1 - num2;
+//     }
+
+//     multiplicacao(num1, num2) {
+//         return num1 * num2;
+//     }
+// }
+
+// const somar = new Matematica();
+
+// console.log(somar.soma(2, 3));
+// console.log(somar.subtracao(2, 3));
+// console.log(somar.multiplicacao(2, 3));
+
+const cliente = {
+    nome: "Marcelo",
+    sobrenome: "Oliveira",
+    rg: "111111111",
+    cpf: "4444444444"
+}
+
+console.log(cliente);
